56320460b8380b94810f82dba3e605b5148d6a7e
Aria <ariabaharmast@gmail.com>
2023-08-01 20:02:14 +0430

    add condition codes to py file 1

--- hello.py
+++ hello.py
@@ -5,3 +5,23 @@ print(name)
 
 print("Hello," + name + " how u doin")
 print(f"Hello, {name} how u doin")
+
+
+if name == 'Aria':
+    print("you are hired")
+
+else:
+    print("you are not hired")
+
+n = int(input('number: '))
+
+
+if n > 0:
+    print("number is positive")
+
+elif n == 0:
+    print("number is zero")
+
+
+else:
+    print("number is negetve")
