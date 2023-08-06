4ef9a17f385fdc096c09f22c5ccf3fd4bcb094de
Aria <ariabaharmast@gmail.com>
2023-08-06 14:08:22 +0430

    complete sequence file

--- sequence.py
+++ sequence.py
@@ -3,5 +3,32 @@ name = 'Ariuf'
 print(name[0])
 
 #list
-names = ['Aria', 'rosha', 'leila']
+names = ['Aria', 'Rosha', 'Leila']
+names.append('Ali')
+names.sort()
 print(names)
+
+#tuple : mostly for saving cordinates of a point or obj ...
+point = (12.5, 25)
+print(point)
+
+#set : Only unique values
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(3)
+s.add(7)
+s.add(5)
+s.add(4)
+s.add(1)
+print(s)
+
+#dict : collection of key-valus pairs
+person = {
+    'name' : 'Aria',
+    'age' : 20,
+    'loc' : 'iran'
+}
+
+print(person)
